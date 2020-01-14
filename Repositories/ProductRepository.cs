ec644bcf17c4a2a63eca4e597e94b40b296a723b
Raluca Miclea <rmiclea02@gmail.com>
2020-01-14 16:22:58 +0200

    product repository

---
+++ Repositories/ProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using GraphQLWebShop.Data;
+using GraphQLWebShop.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraphQLWebShop.Repositories
+{
+    public class ProductRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public ProductRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Task<List<Product>> GetAll()
+        {
+            return _dbContext.Products.ToListAsync();
+        }
+    }
+}
