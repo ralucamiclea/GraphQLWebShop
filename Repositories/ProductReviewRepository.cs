46d811366f6f3d56aa3e7b76788bee60d3804255
Raluca Miclea <rmiclea02@gmail.com>
2020-01-16 11:56:35 +0200

    product review repository

---
+++ Repositories/ProductReviewRepository.cs
@@ -0,0 +1,32 @@
+﻿using GraphQLWebShop.Data;
+using GraphQLWebShop.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraphQLWebShop.Repositories
+{
+    public class ProductReviewRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public ProductReviewRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<ProductReview>> GetForProduct(int productId)
+        {
+            return await _dbContext.ProductReviews.Where(pr => pr.ProductId == productId).ToListAsync();
+        }
+
+        public async Task<ProductReview> Addreview(ProductReview review)
+        {
+            _dbContext.ProductReviews.Add(review);
+            await _dbContext.SaveChangesAsync();
+            return review;
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -29,6 +29,7 @@ namespace GraphQLWebShop
                 options.UseSqlServer(_config["ConnectionStrings:WebShop"])
             );
             services.AddScoped<ProductRepository>();
+            services.AddScoped<ProductReviewRepository>();
 
             services.AddScoped<IDependencyResolver>(s => new FuncDependencyResolver(s.GetRequiredService));
             services.AddScoped<WebShopSchema>();
